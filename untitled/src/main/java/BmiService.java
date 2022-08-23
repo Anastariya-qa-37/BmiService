f683b212ae2f6843a5c96546dda7b6e5e8055cc5
Anastasya <blum1098@yandex.ru>
2022-08-23 20:53:23 +0300

    add

---
+++ untitled/src/main/java/BmiService.java
@@ -0,0 +1,5 @@
+public class BmiService {
+    public double calculate (double mass, double heigt) {
+        return (mass/Math.pow(heigt,2));
+    }
+}
---
+++ untitled/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weight = 75.4;
+        double height = 1.85;
+        double bmi = service.calculate(weight,height);
+        System.out.println(bmi);
+    }
+
+}
